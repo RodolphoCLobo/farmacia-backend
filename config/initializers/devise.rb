de912200059c676ccbc0b609a7cd470b89d6ae3d
Rodolpho Lobo <rodolpho.lobo@achievemore.com.br>
2025-07-13 15:53:26 -0300

    Fix: lint warnigns.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,12 +11,12 @@
 Devise.setup do |config|
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key || ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key || ENV["DEVISE_JWT_SECRET_KEY"]
     jwt.dispatch_requests = [
-      ['POST', %r{^/users/sign_in$}]
+      [ "POST", %r{^/users/sign_in$} ]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/users/sign_out$}]
+      [ "DELETE", %r{^/users/sign_out$} ]
     ]
     jwt.expiration_time = 1.day.to_i
   end
